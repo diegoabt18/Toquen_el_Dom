af7350f73d0d213ee72dde2225a835dae6332d01
diegoabt18 <diegoalbertbt@gmail.com>
2022-01-24 15:46:23 -0500

    Nabvar responsive

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,5 +1,13 @@
 /* JS DIEGO*/
-
+function OpenMenu() {
+    
+    var x = document.getElementById("MyOpenMenu");
+    if (x.className === "close") {
+        x.className = "open";
+    } else {
+        x.className = "close";
+    }
+}
 /* FIN JS DIEGO*/
 
 
