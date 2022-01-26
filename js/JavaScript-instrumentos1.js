57d831c765a190a9f97cee77e721cfa6fa7c7324
JocelynRincon <jocelyn_vanessa@hotmail.com>
2022-01-26 12:04:59 -0500

    sonidos

--- js/JavaScript-instrumentos1.js
+++ js/JavaScript-instrumentos1.js
@@ -6,44 +6,44 @@ function myfuncion(){
      console.log("que hay aqui"+ t);
 }
 
-const c4= new Audio("sonidos/lomographicmusic__piano-07-c4.wav");
-const c4s= new Audio("sonidos/lomographicmusic__piano-08-c-4.wav");
-const d4= new Audio("sonidos/lomographicmusic__piano-09-d4.wav");
-const d4s= new Audio("sonidos/lomographicmusic__piano-11-d-4.wav");
-const e4= new Audio("sonidos/lomographicmusic__piano-13-e4.wav");
-const f4= new Audio("sonidos/lomographicmusic__piano-15-f4.wav");
-const f4s= new Audio("sonidos/lomographicmusic__piano-18-f-4.wav");
-const g4= new Audio("sonidos/lomographicmusic__piano-19-g4.wav");
-const g4s= new Audio("sonidos/lomographicmusic__piano-22-g-4.wav");
-const a4= new Audio("sonidos/lomographicmusic__piano-23-a4.wav");
-const a4s= new Audio("sonidos/lomographicmusic__piano-24-a-4.wav");
-const b4= new Audio("sonidos/lomographicmusic__piano-25-b4.wav");
+const c4= new Audio("sonidos/tesabob2001__c4-middle-c.mp3");
+const c4s= new Audio("sonidos/tesabob2001__c-4.mp3");
+const d4= new Audio("sonidos/tesabob2001__d4.mp3");
+const d4s= new Audio("sonidos/tesabob2001__d-4.mp3");
+const e4= new Audio("sonidos/tesabob2001__e4.mp3");
+const f4= new Audio("sonidos/tesabob2001__f4.mp3");
+const f4s= new Audio("sonidos/tesabob2001__f-4.mp3");
+const g4= new Audio("sonidos/tesabob2001__g4.mp3");
+const g4s= new Audio("sonidos/tesabob2001__g-4.mp3");
+const a4= new Audio("sonidos/tesabob2001__a4.mp3");
+const a4s= new Audio("sonidos/tesabob2001__a-4.mp3");
+const b4= new Audio("sonidos/tesabob2001__b4.mp3");
 
-const c5= new Audio("sonidos/lomographicmusic__piano-26-c5.wav");
-const c5s= new Audio("sonidos/lomographicmusic__piano-31-c-5.wav");
-const d5= new Audio("sonidos/lomographicmusic__piano-36-d5.wav");
-const d5s= new Audio("sonidos/lomographicmusic__piano-40-d-5.wav");
-const e5= new Audio("sonidos/lomographicmusic__piano-41-e5.wav");
-const f5= new Audio("sonidos/lomographicmusic__piano-42-f5.wav");
-const f5s= new Audio("sonidos/lomographicmusic__piano-43-f-5.wav");
-const g5= new Audio("sonidos/lomographicmusic__piano-44-g5.wav");
-const g5s= new Audio("sonidos/lomographicmusic__piano-45-g-5.wav");
-const a5= new Audio("sonidos/lomographicmusic__piano-46-a5.wav");
-const a5s= new Audio("sonidos/lomographicmusic__piano-47-a-5.wav");
-const b5= new Audio("sonidos/lomographicmusic__piano-50-b5.wav");
+const c5= new Audio("sonidos/tesabob2001__c5.mp3");
+const c5s= new Audio("sonidos/tesabob2001__c-5.mp3");
+const d5= new Audio("sonidos/tesabob2001__d5.mp3");
+const d5s= new Audio("ssonidos/tesabob2001__d-5.mp3");
+const e5= new Audio("sonidos/tesabob2001__e5.mp3");
+const f5= new Audio("sonidos/tesabob2001__f5.mp3");
+const f5s= new Audio("sonidos/tesabob2001__f-5.mp3");
+const g5= new Audio("sonidos/tesabob2001__g5.mp3");
+const g5s= new Audio("sonidos/tesabob2001__g-5.mp3");
+const a5= new Audio("sonidos/tesabob2001__a5.mp3");
+const a5s= new Audio("sonidos/tesabob2001__a-5.mp3");
+const b5= new Audio("sonidos/tesabob2001__b5.mp3");
 
-const c6= new Audio("sonidos/lomographicmusic__piano-51-c6.wav");
-const c6s= new Audio("sonidos/lomographicmusic__piano-52-c-6.wav");
-const d6= new Audio("sonidos/lomographicmusic__piano-53-d6.wav");
-const d6s= new Audio("sonidos/lomographicmusic__piano-54-d-6.wav");
-const e6= new Audio("sonidos/lomographicmusic__piano-55-d-6.wav");
-const f6= new Audio("sonidos/lomographicmusic__piano-56-g-6.wav");
-const f6s= new Audio("sonidos/lomographicmusic__piano-57-g-6.wav");
-const g6= new Audio("sonidos/lomographicmusic__piano-58-a6.wav");
-const g6s= new Audio("sonidos/lomographicmusic__piano-59-a6.wav");
-const a6= new Audio("sonidos/lomographicmusic__piano-60-a6.wav");
-const a6s= new Audio("sonidos/lomographicmusic__piano-61-a-6.wav");
-const b6= new Audio("sonidos/lomographicmusic__piano-62-a-6.wav");
+const c3= new Audio("sonidos/tesabob2001__c3.mp3");
+const c3s= new Audio("sonidos/tesabob2001__c-3.mp3");
+const d3= new Audio("sonidos/tesabob2001__d3.mp3");
+const d3s= new Audio("sonidos/tesabob2001__d-3.mp3");
+const e3= new Audio("sonidos/tesabob2001__e3.mp3");
+const f3= new Audio("sonidos/tesabob2001__f3.mp3");
+const f3s= new Audio("sonidos/tesabob2001__f-3.mp3");
+const g3= new Audio("sonidos/tesabob2001__g3.mp3");
+const g3s= new Audio("sonidos/tesabob2001__g-3.mp3");
+const a3= new Audio("sonidos/tesabob2001__a3.mp3");
+const a3s= new Audio("sonidos/tesabob2001__a-3.mp3");
+const b3= new Audio("sonidos/tesabob2001__b3.mp3");
 
 const reproducirNota = audio => {
     const clone = audio.cloneNode();
@@ -59,6 +59,19 @@ const tocarTecla = (idTecla, nota) => {
 };
 
 const teclas = [
+    { id: ".c3", nota: c3, key: 97 },
+    { id: ".c3s", nota: c3s, key: 100 },
+    { id: ".d3", nota: d3, key: 98 },
+    { id: ".d3s", nota: d3s, key: 101 },
+    { id: ".e3", nota: e3, key: 99 },
+    { id: ".f3", nota: f3, key: 102 },
+    { id: ".f3s", nota: f3s, key: 103 },
+    { id: ".g3", nota: g3, key: 107 },
+    { id: ".g3s", nota: g3s, key: 104 },
+    { id: ".a3", nota: a3, key: 10 },
+    { id: ".a3s", nota: a3s, key: 105 },
+    { id: ".b3", nota: b3, key: 111 },
+    
     { id: ".c4", nota: c4, key: 81 },
     { id: ".c4s", nota: c4s, key: 50 },
     { id: ".d4", nota: d4, key: 87 },
@@ -85,18 +98,7 @@ const teclas = [
     { id: ".a5s", nota: a5s, key: 67 },
     { id: ".b5", nota: b5, key: 59 },
 
-    { id: ".c6", nota: c6, key: 97 },
-    { id: ".c6s", nota: c6s, key: 100 },
-    { id: ".d6", nota: d6, key: 98 },
-    { id: ".d6s", nota: d6s, key: 101 },
-    { id: ".e6", nota: e6, key: 99 },
-    { id: ".f6", nota: f6, key: 102 },
-    { id: ".f6s", nota: f6s, key: 103 },
-    { id: ".g6", nota: g6, key: 107 },
-    { id: ".g6s", nota: g6s, key: 104 },
-    { id: ".a6", nota: a6, key: 10 },
-    { id: ".a6s", nota: a6s, key: 105 },
-    { id: ".b6", nota: b6, key: 111 },
+   
 ];
 
 
